8a2467f995e55768e8261ecf89dd2dd223c8a5f2
sasidhar22111998 <sasidhar.chennupalli509@gmail.com>
2024-12-14 14:27:51 +0530

    exceptions folder modified

--- template.py
+++ template.py
@@ -19,7 +19,7 @@ list_of_files=[
     "src\\utils\\__init__.py",
     "src\\utils\\utils.py",
     "src\\logger\\logging.py",
-    "src\\exception\\exceptions",
+    "src\\exception\\exceptions.py",
     "tests\\unit\\__init__.py",
     "tests\\integration\\__init__.py",
     "init_setup.sh",
